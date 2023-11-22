2ad8d0a87b7311b119dbc5d7b882e1274cf10d12
sofianes78 <sofiane.serraripro@gmail.com>
2023-11-22 12:02:13 +0100

    clear project folder

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import Counter from "./components/Counter";
-import SayHi from "./components/SayHi";
-import {useState} from 'react';
-import "./App.css"
+
 function App() {
-      const [count, setCount ] = useState(0);
+    
 
 return (
     <div>
-     
-
-      <div className="bg-red-400">
-        <button
-          onClick={() => setCount(count + 1)}>
-          +
-        </button>
-        <p style={{fontSize: '30px', color: 'white'}}>{count}</p>
-        <button>-</button>
-      </div>
     </div>
-  );}
+  );
+}
 export default App;
